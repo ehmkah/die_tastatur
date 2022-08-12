db17b6d4345e75e77915c4ef52901b52ce1211b3
Michael Krausse <michael.krausse@directbox.com>
2022-08-12 21:47:17 +0200

    shortcut implemented

--- die-tastatur-backend/src/executions/special_keys.ts
+++ die-tastatur-backend/src/executions/special_keys.ts
@@ -1,6 +1,7 @@
 // Used for defining special keys like ctrl, shift and co
 export enum SpecialKeys {
     SHIFT = 'SHIFT',
+    ALT = 'ALT',
     CTRL = 'CTRL',
     SPACE = 'SPACE',
     RETURN = 'RETURN',
