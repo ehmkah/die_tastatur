e0a7b38a226e25e3a54d615df4e0cf953dc9ead0
Michael Krausse <michael.krausse@directbox.com>
2022-09-01 10:20:52 +0200

    identify buttons better in print screen

--- die-tastatur-backend/src/keyboard-layouts/keyboard-layouts.controller.ts
+++ die-tastatur-backend/src/keyboard-layouts/keyboard-layouts.controller.ts
@@ -39,15 +39,37 @@ export class KeyboardLayoutsController {
     // wokrs only on chrome
     mapToHTML(keyboardLayout: KeyboardLayout) {
         let result = '<table style="border: 1px solid black;border-collapse: collapse;">';
+        //result = result + `<tr><td>0</td></tr>
+        const letters=['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+        let rowCounter = 0;
+        const constColumNumber = keyboardLayout.keys[0].length;
+        let columnCounter = 0;
+        result = result + '<tr style="border: 1px solid black;border-collapse: collapse;"><td></td>';
+        while(columnCounter < constColumNumber) {
+            result = result + `<td style="border: 1px solid black;border-collapse: collapse;">${columnCounter}</td>`
+            columnCounter++;
+        }
+        result = result + '</tr>';
         for (const row of keyboardLayout.keys) {
             result = result + '<tr style="border: 1px solid black;border-collapse: collapse;">';
+            result = result + `<td>${rowCounter}</td>`
             for (const key of row) {
                 result = result + '<td style="border: 1px solid black;border-collapse: collapse;">';
                 result = result + key.execution.command.defaultKeyPrintText;
                 result = result + "</td>";
             }
+            result = result + `<td>${letters[rowCounter]}</td>`;
             result = result + "</tr>";
+            rowCounter++;
         }
+        columnCounter = columnCounter;
+        result = result + '<tr style="border: 1px solid black;border-collapse: collapse;"><td></td>';
+        while(columnCounter > 0) {
+            result = result + `<td style="border: 1px solid black;border-collapse: collapse;">${columnCounter}</td>`
+            columnCounter--;
+        }
+
+
         result = result + '</table>';
         return result;
     }
