3013b12dd54d7f4e911849a492d37ded6c80b39b
Michael Krausse <michael.krausse@directbox.com>
2022-12-08 19:31:23 +0100

    feature: toggle column selection mode : bugfix previous and next method

--- die-tastatur-backend/src/keyboard-layouts/keyboard-layouts.service.spec.ts
+++ die-tastatur-backend/src/keyboard-layouts/keyboard-layouts.service.spec.ts
@@ -38,10 +38,10 @@ describe('KeyboardLayoutsService', () => {
     expect(actual[0].keys.length).toEqual(8);
     expect(actual[0].keys[0].length).toEqual(16);
     expect(actual[0].keys[0][0].execution.command.name).toEqual('ide_search_and_replace_find');
-    expect(actual[0].keys[7][15].execution.command.name).toEqual('IDE_VCS_UPDATE_TO_VCS');
+    expect(actual[0].keys[7][15].execution.command.name).toEqual('ide_vcs_commit_to_vcs');
     expect(actual[0].name).toEqual('Intellij classic Linux.testkeyboardlayout0');
     expect(actual[1].id).toEqual("Intellij classic Linux.testkeyboardlayout1");
-    expect(actual[1].keys[0][0].execution.command.name).toEqual('IDE_VCS_PUSH_TO_VCS');
+    expect(actual[1].keys[0][0].execution.command.name).toEqual('IDE_VCS_UPDATE_TO_VCS');
     expect(actual[1].keys[7][15].execution.command.name).toEqual('empty');
   });
 
