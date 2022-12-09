425a86a97eb7fe321671fb32152dc36213db1268
Michael Krausse <michael.krausse@directbox.com>
2022-12-09 22:18:01 +0100

    feature: support mci-96 : search for keyboardlayout id

--- die-tastatur-backend/src/keyboards/keyboards.service.ts
+++ die-tastatur-backend/src/keyboards/keyboards.service.ts
@@ -1,14 +1,27 @@
 import {Injectable} from '@nestjs/common';
 import {Keyboard} from "./keyboard.interface";
 
+export enum KeyboardIds {
+    PREHKEYTECH_128 = 'Prekeytech 128',
+    PREHKEYTECH_96 = 'Prekeytech 96',
+}
+
 @Injectable()
 export class KeyboardsService {
 
+
     findAll(): Array<Keyboard> {
         return [
-            <Keyboard>{id: 'Prekeytech 128', xSize: 16, ySize: 8},
-            <Keyboard>{id: 'Prekeytech 96',  xSize: 16, ySize: 6}
+            <Keyboard>{id: KeyboardIds.PREHKEYTECH_128, xSize: 16, ySize: 8},
+            <Keyboard>{id: KeyboardIds.PREHKEYTECH_96,  xSize: 16, ySize: 6}
         ]
+    }
+
+    findById(keyboardId : KeyboardIds) {
+        const keyboards = this.findAll();
+        const result = keyboards.find(value => value.id === keyboardId);
+
+        return result;
     }
 
 }
