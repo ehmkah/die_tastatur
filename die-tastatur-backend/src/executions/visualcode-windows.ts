2c25256dd2ae19958a4d1d68e1f20ab134169032
Michael Krausse <michael.krausse@directbox.com>
2022-12-26 21:04:00 +0100

    feature: visual studio - first round

---
+++ die-tastatur-backend/src/executions/visualcode-windows.ts
@@ -0,0 +1,270 @@
+Ctrl+Shift+P, F1   Show Command Palette
+Ctrl+P   Quick Open, Go to File...
+Ctrl+Shift+N   New window/instance
+Ctrl+Shift+W   Close window/instance
+Ctrl+,   User Settings
+Ctrl+K Ctrl+S   Keyboard Shortcuts
+
+
+Ctrl+X   Cut line (empty selection)
+Ctrl+C   Copy line (empty selection)
+Alt+ ↑ / ↓   Move line up/down
+Shift+Alt + ↓ / ↑   Copy line up/down
+Ctrl+Shift+K   Delete line
+Ctrl+Enter   Insert line below
+Ctrl+Shift+Enter   Insert line above
+Ctrl+Shift+\   Jump to matching bracket
+Ctrl+] / [   Indent/outdent line
+Home / End   Go to beginning/end of line
+Ctrl+Home   Go to beginning of file
+Ctrl+End   Go to end of file
+Ctrl+↑ / ↓   Scroll line up/down
+Alt+PgUp / PgDn   Scroll page up/down
+Ctrl+Shift+[   Fold (collapse) region
+Ctrl+Shift+]   Unfold (uncollapse) region
+Ctrl+K Ctrl+[   Fold (collapse) all subregions
+Ctrl+K Ctrl+]   Unfold (uncollapse) all subregions
+Ctrl+K Ctrl+0   Fold (collapse) all regions
+Ctrl+K Ctrl+J   Unfold (uncollapse) all regions
+Ctrl+K Ctrl+C   Add line comment
+Ctrl+K Ctrl+U   Remove line comment
+Ctrl+/   Toggle line comment
+Shift+Alt+A   Toggle block comment
+Alt+Z   Toggle word wrap
+
+
+Ctrl+T   Show all Symbols
+Ctrl+G   Go to Line...
+Ctrl+P   Go to File...
+Ctrl+Shift+O   Go to Symbol...
+Ctrl+Shift+M   Show Problems panel
+F8   Go to next error or warning
+Shift+F8   Go to previous error or warning
+Ctrl+Shift+Tab   Navigate editor group history
+Alt+ ← / →   Go back / forward
+Ctrl+M Toggle Tab moves focus
+
+Ctrl+F   Find
+Ctrl+H   Replace
+F3 / Shift+F3   Find next/previous
+Alt+Enter   Select all occurences of Find match
+Ctrl+D   Add selection to next Find match
+Ctrl+K Ctrl+D   Move last selection to next Find match
+Alt+C / R / W   Toggle case-sensitive / regex / whole word
+
+Alt+Click  Insert cursor
+Ctrl+Alt+ ↑ / ↓  Insert cursor above / below
+Ctrl+U  Undo last cursor operation
+Shift+Alt+I  Insert cursor at end of each line selected
+Ctrl+L  Select current line
+Ctrl+Shift+L  Select all occurrences of current selection
+Ctrl+F2  Select all occurrences of current word
+Shift+Alt+→  Expand selection
+Shift+Alt+←  Shrink selection
+Shift+Alt + (drag mouse)  Column (box) selection
+Ctrl+Shift+Alt +PgUp/PgDn  Column (box) selection page up/down
+Ctrl+Shift+Alt Column (box) selection + (arrow key)
+
+Ctrl+Space, Ctrl+I  Trigger suggestion
+Ctrl+Shift+Space  Trigger parameter hints
+Shift+Alt+F  Format document
+Ctrl+K Ctrl+F  Format selection
+F12  Go to Definition
+Alt+F12  Peek Definition
+Ctrl+K F12  Open Definition to the side
+Ctrl+.  Quick Fix
+Shift+F12  Show References
+F2  Rename Symbol
+Ctrl+K Ctrl+X  Trim trailing whitespace
+Ctrl+K M  change file lamnguage
+
+Ctrl+F4, Ctrl+W
+Ctrl+K F
+Ctrl+\
+Ctrl+ 1 / 2 / 3
+Ctrl+K Ctrl+ ←/→
+Close editor
+Close folder
+Split editor
+Focus into 1st, 2nd or 3rd editor group
+Focus into previous/next editor group
+Ctrl+Shift+PgUp/PgDn Moveeditorleft/right
+Ctrl+K ← / → Move active editor group
+
+Ctrl+N  New File
+Ctrl+O  Open File...
+Ctrl+S  Save
+Ctrl+Shift+S    Save As...
+Ctrl+K S    Save All
+Ctrl+F4 Close
+Ctrl+K Ctrl+W   Close ALl
+Ctrl+Shift+T    Reopen closed editor
+Ctrl+K Enter    Keep preview mode editor open
+Ctrl+Tab    Open next
+Ctrl+Shift+Tab  Open previous
+Ctrl+K P    Copy path of active file
+Ctrl+K R    Reveeal active file in explorer
+Ctrl+K O    Show active file in new window/instance
+
+
+
+
+
+
+
+
+Shift+Alt+0 Ctrl+B Ctrl+Shift+F Ctrl+Shift+D Ctrl+Shift+H Ctrl+Shift+U Ctrl+K V
+Debug
+F5
+F11 / Shift+F11
+Open File...
+Save As...
+Close
+Reopen closed editor Open next
+Copy path of active file
+Show active file in new window/instance
+Toggle editor layout (horizontal/vertical) Toggle Sidebar visibility
+Show Search
+Show Debug
+Replace in files
+Show Output panel
+Open Markdown preview to the side
+Start/Continue Step into/out Show hover
+Ctrl+S
+Save
+General
+Ctrl+Shift+P, F1
+Ctrl+P
+Ctrl+Shift+N
+Ctrl+Shift+W
+Ctrl+,
+Ctrl+K Ctrl+S Basic editing
+Ctrl+X
+Ctrl+C
+Alt+ ↑ / ↓
+Shift+Alt + ↓ / ↑
+Ctrl+Shift+K
+Ctrl+Enter
+Ctrl+Shift+Enter
+Ctrl+Shift+\
+Ctrl+] / [
+    Home / End
+    Ctrl+Home
+    Ctrl+End
+    Ctrl+↑ / ↓
+Alt+PgUp / PgDn
+Ctrl+Shift+[
+    Ctrl+Shift+]
+Ctrl+K Ctrl+[
+    Ctrl+K Ctrl+]
+Ctrl+K Ctrl+0
+Ctrl+K Ctrl+J
+Ctrl+K Ctrl+C
+Ctrl+K Ctrl+U
+Ctrl+/
+Shift+Alt+A
+Alt+Z
+Navigation
+Ctrl+T
+Ctrl+G
+Ctrl+P
+Ctrl+Shift+O
+Ctrl+Shift+M
+F8
+Shift+F8
+Ctrl+Shift+Tab
+Alt+ ← / →
+Ctrl+F
+Ctrl+H
+F3 / Shift+F3
+Alt+Enter
+Ctrl+D
+Ctrl+K Ctrl+D
+Alt+C / R / W
+Find
+Replace
+Find next/previous
+Select all occurences of Find match
+Add selection to next Find match
+Move last selection to next Find match
+Toggle case-sensitive / regex / whole word
+Ctrl+K S
+Save All
+Ctrl+K Ctrl+W
+Close All
+Ctrl+K Enter
+Keep preview mode editor open
+Multi-cursor and selection
+Ctrl+Shift+Tab
+Open previous
+Alt+Click
+Ctrl+Alt+ ↑ / ↓
+Ctrl+U
+Shift+Alt+I
+Ctrl+L
+Ctrl+Shift+L
+Ctrl+F2
+Shift+Alt+→
+Shift+Alt+←
+Shift+Alt + (drag mouse)
+Ctrl+Shift+Alt +PgUp/PgDn
+Insert cursor
+Insert cursor above / below
+Undo last cursor operation
+Insert cursor at end of each line selected
+Select current line
+Select all occurrences of current selection
+Select all occurrences of current word
+Expand selection
+Shrink selection
+Column (box) selection
+Column (box) selection page up/down
+Ctrl+K R
+Reveal active file in Explorer
+F11
+Toggle full screen
+Ctrl+ = / -
+Zoom in/out
+Ctrl+Shift+E
+Show Explorer / Toggle focus
+Ctrl+Shift+G
+Show Source Control
+Ctrl+Shift+Alt Column (box) selection + (arrow key)
+Ctrl+Shift+X
+Show Extensions
+Ctrl+Shift+J
+Toggle Search details
+Rich languages editing
+Ctrl+Shift+V
+Open Markdown preview
+Ctrl+Space, Ctrl+I
+Ctrl+Shift+Space
+Shift+Alt+F
+Ctrl+K Ctrl+F
+F12
+Alt+F12
+Ctrl+K F12
+Ctrl+.
+Shift+F12
+F2
+Ctrl+K Ctrl+X
+Ctrl+K M
+Editor management
+Trigger suggestion
+Trigger parameter hints
+Format document
+Format selection
+Go to Definition
+Peek Definition
+Open Definition to the side
+Quick Fix
+Show References
+Rename Symbol
+Trim trailing whitespace
+Ctrl+K Ctrl+I Integrated terminal
+Ctrl+K Z
+Zen Mode (Esc Esc to exit)
+
+
+
+
